d7e9fbf9d5647236a85a71969943e4db0cec9c4a
baotran <brightsmile959674@gmail.com>
2018-11-30 18:01:55 +0700

    Add node class and tests

--- Demo/GeneticProgrammingDemo/Node.cs
+++ Demo/GeneticProgrammingDemo/Node.cs
@@ -1,40 +1,105 @@
 ﻿using System;
 
 /*
- * 
- *          Node (Terminal or Function)
- *        /     \
- *      Left    Right
- * 
+         Node (Terminal or Function)
+        /     \
+      Left    Right
+
+Terminal: Float values
+Function: +, -, *, sin , cos
+
  */
 namespace GeneticProgrammingDemo
 {
-    public class Node
-    {
-		public enum Type {
-			FUNCTION, 
-            TERMINAL
+	public class Node
+	{
+		public enum Function
+		{
+			ADD,
+			SUBTRACT,
+			MULTIPLY,
+			SIN, // right = null
+			COS, // right = null
+		}
+
+		public enum Type
+		{
+			FUNCTION,
+			TERMINAL
 		}
 
 		private Type type = Type.TERMINAL;
-		private double value;
+		private Function funtion = Function.ADD; // If not leaft node
+		private double value; // If leaft node
 		private Node left;
 		private Node right;
 
-        public Node()
-        {
-        }
+		public Node(Type type, double value)
+		{
+			this.type = type;
+			this.value = value;
+		}
 
-        /*
+		public Node(Type type, Function function, Node left, Node right = null)
+		{
+			this.type = type;
+			this.funtion = function;
+			this.left = left;
+			this.right = right;
+		}
+
+		/*
          * Get result of tree
          */
-		public double getResult() 
+		public double getResult()
 		{
-			if (type == Type.TERMINAL) {
-				return value;
-			} else {
-				return left.getResult() + right.getResult();
+			double result = 0;
+			switch (type)
+			{
+				case Type.TERMINAL: // left = null, right = null
+					result = this.value;
+					break;
+				case Type.FUNCTION: // left != null
+					if (left != null && right != null)
+					{
+						switch (funtion)
+						{
+							case Function.ADD:
+								result = left.getResult() + right.getResult();
+								break;
+							case Function.SUBTRACT:
+								result = left.getResult() - right.getResult();
+								break;
+							case Function.MULTIPLY:
+								result = left.getResult() * right.getResult();
+								break;
+							default:
+								result = 0;
+								break;
+						}
+
+					}
+					else if (left != null && right == null)
+					{
+						switch (funtion)
+						{
+							case Function.SIN:
+								result = Math.Sin(left.getResult() * Math.PI / 180.0);
+								break;
+							case Function.COS:
+								result = Math.Cos(left.getResult() * Math.PI / 180.0);
+								break;
+							default:
+								result = 0;
+								break;
+						}
+					}
+					break;
+				default:
+					result = 0;
+					break;
 			}
+			return result;
 		}
-    }
+	}
 }
--- Demo/GeneticProgrammingDemo/Program.cs
+++ Demo/GeneticProgrammingDemo/Program.cs
@@ -2,11 +2,103 @@
 
 namespace GeneticProgrammingDemo
 {
-    class MainClass
-    {
-        public static void Main(string[] args)
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			
+		}
+
+
+
+		// ==================== NODE TEST =======================
+		/*
+         *    F = sin(180) + 12 - (-2.2 * 13) = 40.6
+         * 
+         *                         + (A)     <--- Level 0
+         *                       /   \
+		 *                  sin (B)    - (C)    
+         *                    /       /   \
+		 *              180 (D)   12 (E)  * (F) 
+         *                               /   \
+		 *                          -2.2 (G)  13 (H)
+         * 
+         */
+		public static void testNode1()
+		{
+			Node d = new Node(Node.Type.TERMINAL, 180);
+			Node e = new Node(Node.Type.TERMINAL, 12);
+			Node g = new Node(Node.Type.TERMINAL, -2.2);
+			Node h = new Node(Node.Type.TERMINAL, 13);
+
+			Node f = new Node(Node.Type.FUNCTION, Node.Function.MULTIPLY, g, h);
+			Node c = new Node(Node.Type.FUNCTION, Node.Function.SUBTRACT, e, f);
+			Node b = new Node(Node.Type.FUNCTION, Node.Function.SIN, e);
+			Node a = new Node(Node.Type.FUNCTION, Node.Function.ADD, b, c);
+
+			Console.WriteLine(a.getResult());
+		}
+
+		/*
+         *    F = sin[cos(-200) + [sin(900) * (-3) + (-9)]] = -0.173
+         * 
+         *                       sin (A)     <--- Level 0
+         *                         |
+         *                         + (B)
+         *                       /   \
+         *                  cos (C)    + (D)    
+         *                    /       /   \
+         *              -200 (E)   * (F)  -9 (G) 
+         *                       /   \
+         *                   sin (H)  -3(I)
+         *                      |
+		 *                   900 (K)
+         */
+        public static void testNode2()
         {
-            Console.WriteLine("Hello World!");
+            Node k = new Node(Node.Type.TERMINAL, 900);
+            Node i = new Node(Node.Type.TERMINAL, -3);
+			Node g = new Node(Node.Type.TERMINAL, -9);
+            Node e = new Node(Node.Type.TERMINAL, -200);
+            
+            Node h = new Node(Node.Type.FUNCTION, Node.Function.SIN, k);
+			Node f = new Node(Node.Type.FUNCTION, Node.Function.MULTIPLY, h, i);
+            Node d = new Node(Node.Type.FUNCTION, Node.Function.ADD, f, g);
+			Node c = new Node(Node.Type.FUNCTION, Node.Function.COS, e);
+			Node b = new Node(Node.Type.FUNCTION, Node.Function.ADD, c, d);
+            Node a = new Node(Node.Type.FUNCTION, Node.Function.SIN, b);
+
+            Console.WriteLine(a.getResult());
         }
-    }
+
+		/*
+         *    F1 = sin(-7.2) = -0.125
+         * 
+         *                         sin (A)     <--- Level 0
+         *                       /   
+		 *                    -7.2 (B)
+         */      
+		public static void testNode3()
+		{
+			Node b = new Node(Node.Type.TERMINAL, -7.2);
+			Node a = new Node(Node.Type.FUNCTION, Node.Function.SIN, b);
+
+			Console.WriteLine(a.getResult());
+		}
+
+		/*
+         *    F1 = cos(1000) = 0.173
+         * 
+         *                         cos (A)     <--- Level 0
+         *                       /   
+         *                    1000 (B)
+         */
+        public static void testNode4()
+        {
+            Node b = new Node(Node.Type.TERMINAL, 1000);
+            Node a = new Node(Node.Type.FUNCTION, Node.Function.COS, b);
+            
+            Console.WriteLine(a.getResult());
+        }
+	}
 }
