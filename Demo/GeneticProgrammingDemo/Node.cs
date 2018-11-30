822ee4f4e0bca71eee36d5c474232175d6c29d2a
baotran <brightsmile959674@gmail.com>
2018-11-30 16:28:18 +0700

    Initial commit

---
+++ Demo/GeneticProgrammingDemo/Node.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/*
+ * 
+ *          Node (Terminal or Function)
+ *        /     \
+ *      Left    Right
+ * 
+ */
+namespace GeneticProgrammingDemo
+{
+    public class Node
+    {
+		public enum Type {
+			FUNCTION, 
+            TERMINAL
+		}
+
+		private Type type = Type.TERMINAL;
+		private double value;
+		private Node left;
+		private Node right;
+
+        public Node()
+        {
+        }
+
+        /*
+         * Get result of tree
+         */
+		public double getResult() 
+		{
+			if (type == Type.TERMINAL) {
+				return value;
+			} else {
+				return left.getResult() + right.getResult();
+			}
+		}
+    }
+}
---
+++ Demo/GeneticProgrammingDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeneticProgrammingDemo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
